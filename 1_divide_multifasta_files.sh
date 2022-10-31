1018021d9b3d334a7d5f72486a6f4b1baacea0a1
SvetlanaAliukas <117092715+SvetlanaAliukas@users.noreply.github.com>
2022-10-31 23:06:23 +0300

    Update 1_divide_multifasta_files.sh

--- 1_divide_multifasta_files.sh
+++ 1_divide_multifasta_files.sh
@@ -4,10 +4,10 @@
 #!/bin/bash
 
 read -p 'Type the folder name that contain your multifasta file: ' foldername_file
-read -p 'Type the format of the multifata file. Type with the dot, for example .fna or .fa or .fasta : ' format
+read -p 'Type the format of the multifasta file. Type with the dot, for example .fna or .fa or .fasta : ' format
 
 
 for f in $foldername_file/*$format ; do awk -v var="$format" '{if(substr($0, 1, 1)==">"){filename=(substr($0,2)var)} print $0 > filename}' "$f" ; done
 
-echo Step successfully done! Go to the nest step!
+echo Step successfully done! Go to the next step!
 
