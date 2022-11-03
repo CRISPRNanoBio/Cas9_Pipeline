9aaaa1f992df1886b705b511017b2a393f10f20a
SvetlanaAliukas <117092715+SvetlanaAliukas@users.noreply.github.com>
2022-11-03 12:47:13 +0300

    Update 3_cut_crispr_region.py

--- 3_cut_crispr_region.py
+++ 3_cut_crispr_region.py
@@ -22,40 +22,38 @@ for file in glob.glob(format_file_1):
             newstring=line.find('Range: ')
             newstring1=line[newstring:position_coordinate]
             bingo.append(newstring1) #it`s a list that has crispr without digits (of the location crispr-cas)
-            bingo = [x for x in bingo if x != ''] #delete '' from the list bingo (==save everything except '') #НАДО ДОБАВИТЬ ИЗБАВЛЕНИЕ ОТ WARNING!
-            bingo=[y.replace('Range: ', '') for y in bingo]#delete 'Range: ' from the list bingo 
-            bingo=[k.replace('-','') for k in bingo]#delete '-' from the list bingo
-        print(bingo)
+            bingo = [x for x in bingo if x != ''] #delete '' from the list bingo (==save everything except '') 
+            bingo=[y.replace('Range: ', '') for y in bingo] #delete 'Range: ' from the list bingo 
+            bingo=[k.replace('-','') for k in bingo]  #delete '-' from the list bingo
         bingo_new=[]
         for coordinate in bingo:
             bingo_new.append(coordinate.split())  
-        print(bingo_new)    
+            
     with open(file) as file1:
         newline_breaks=""
         for line in file1: 
             stripped_line = line.strip()
             newline_breaks += stripped_line
         dna_len=len(newline_breaks)
-    #print(dna_len)  
+      
     if dna_len>length_ORF*2:
         for coordinate in bingo_new:    
             after= int(coordinate[1])
             before=int(coordinate[0])
-            before_cut=before-length_ORF+len_filename #т.к. когда код будет читать файл, он начнет с > и чтобы это невелировать добавляю len_filename
-            after_cut=after+length_ORF+len_filename #т.к. когда код будет читать файл, он начнет с > и чтобы это невелировать добавляю len_filename
-        #print(after_)
-        #print(before_)
+            before_cut=before-length_ORF+len_filename #start reading file from the second line (exclude 1st line starting with >) 
+            after_cut=after+length_ORF+len_filename         
+        
             
             if before<=length_ORF and after_cut>=dna_len:   
-                result=newline_breaks[len_filename:] #чтобы избежать записи > снова!
+                result=newline_breaks[len_filename:] # write new file without '>' and filename information, just nucleotides 
             elif before>=length_ORF and after_cut<=dna_len:
                 result=newline_breaks[before_cut:after_cut]
             elif before<=length_ORF and after_cut<=dna_len:
                result=newline_breaks[len_filename:after_cut]
             elif before>=length_ORF and after_cut>=dna_len:
                 result=newline_breaks[before_cut:]    
-            #print(result)
-            name=str(file)[:len_format_file]+'_'+'nucl-coordin-of-crispr-'+str(before)+'-'+str(after)+'.fa' #добавляет 
+            
+            name=str(file)[:len_format_file]+'_'+'nucl-coordin-of-crispr-'+str(before)+'-'+str(after)+'.fa' #add to the filename nucleotide positions that were saved
             with open(name, 'w') as f: 
                 f.write('>'+name[:-3]+' \n'+result)
     else:
